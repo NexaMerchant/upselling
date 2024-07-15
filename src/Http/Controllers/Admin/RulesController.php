0079b3e86fa388c9d6ac9d417ddc1cad219d9ed3
Steve <34465153+xxl4@users.noreply.github.com>
2024-07-15 18:22:32 +0800

    add rules

--- src/Http/Controllers/Admin/RulesController.php
+++ src/Http/Controllers/Admin/RulesController.php
@@ -7,6 +7,30 @@ class RulesController extends Controller
 {
     public function index()
     {
-        return view('upselling::admin.rules.index');
+        return view('Upselling::Admin.rules.index');
+    }
+
+    public function create()
+    {
+        return view('Upselling::Admin.rules.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'type' => 'required',
+            'status' => 'required',
+        ]);
+
+        $rule = new Rule();
+        $rule->name = $request->name;
+        $rule->description = $request->description;
+        $rule->type = $request->type;
+        $rule->status = $request->status;
+        $rule->save();
+
+        return redirect()->route('admin.rules.index');
     }
 }
