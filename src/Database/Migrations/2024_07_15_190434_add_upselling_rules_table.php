a662ab46a523ef87fee1a4ba5f24734b6213f301
Steve <34465153+xxl4@users.noreply.github.com>
2024-07-16 09:24:57 +0800

    update upselling rule

--- src/Database/Migrations/2024_07_15_190434_add_upselling_rules_table.php
+++ src/Database/Migrations/2024_07_15_190434_add_upselling_rules_table.php
@@ -15,9 +15,13 @@ return new class extends Migration
         Schema::create('upselling_rules', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
-            $table->string('type');
-            $table->string('status');
+            $table->string('description')->comment('description of the rule')->nullable();
+            $table->chat('type', 50)->comment("type of the rule");
+            $table->tinyInteger('status')->default(1)->comment('status of the rule');
+            $table->json('conditions')->nullable()->comment('conditions rules');
+            $table->float('discount', 8, 4)->default(0)->comment('discount of the rule');
+            $table->tinyInteger('sort_order')->default(0)->comment('sort order'); 
+            $table->tinyInteger('is_end')->default(0)->comment('is end of the rule');
             $table->timestamps();
         });
     }
