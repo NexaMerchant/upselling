cb3cbd44fbe8d1c600a6a06b9a951fbf300ac36e
Steve <34465153+xxl4@users.noreply.github.com>
2024-07-16 11:58:29 +0800

    fix add upselling order docs

--- src/Docs/V1/Upselling/Controllers/Orders/OrdersController.php
+++ src/Docs/V1/Upselling/Controllers/Orders/OrdersController.php
@@ -5,7 +5,7 @@ class OrdersController {
 
     /**
      * @OA\Get(
-     *      path="/api/v1/orders",
+     *      path="/api/upselling/v1/orders",
      *      operationId="getOrders",
      *      tags={"Orders"},
      *      summary="Get list of orders",
@@ -76,4 +76,123 @@ class OrdersController {
      */
     public function getOrders() {
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/upselling/v1/orders/{id}",
+     *      operationId="getOrderById",
+     *      tags={"Orders"},
+     *      summary="Get order information",
+     *      description="Returns order data",
+     *
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Order id",
+     *          required=true,
+     *          in="path",
+     *
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource not found"
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     * )
+     */
+    public function getOrderById($id) {
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/upselling/v1/orders",
+     *      operationId="createOrder",
+     *      tags={"Orders"},
+     *      summary="Create new order",
+     *      description="Creates a new order",
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request"
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     * )
+     */
+    public function createOrder() {}
+
+    /**
+     * @OA\Put(
+     *      path="/api/upselling/v1/orders/{id}",
+     *      operationId="updateOrder",
+     *      tags={"Orders"},
+     *      summary="Update existing order",
+     *      description="Updates an existing order",
+     *
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Order id",
+     *          required=true,
+     *          in="path",
+     *
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request"
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource not found"
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     * )
+     */
+    public function updateOrder($id) {}
 }
