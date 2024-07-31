ff0480f28554f140e33bc25c84ca9a33f35fc5e5
Steve <34465153+xxl4@users.noreply.github.com>
2024-07-31 18:50:27 +0800

    fix

--- src/Upselling.php
+++ src/Upselling.php
@@ -18,6 +18,7 @@ use Webkul\Checkout\Models\CartPayment;
 use Webkul\Checkout\Models\CartAddress;
 use Webkul\Tax\Helpers\Tax;
 use Illuminate\Support\Facades\Log;
+use NexaMerchant\Apis\Docs\V1\Shop\Models\Customer\CartItem;
 
 class Upselling {
 
@@ -41,5 +42,56 @@ class Upselling {
         $this->cart = $cart;
         // Apply upselling logic here
         //Log::info('Upselling logic applied '.json_encode($cart));
+
+        // // Get cart items
+        // $cartItems = $this->cartItemRepository->findWhere(['cart_id' => $this->cart->id]);
+
+        // // Get cart total
+        // $cartTotal = $this->cart->grand_total;
+
+        // // Get cart total quantity
+
+        // $cartTotalQuantity = 0;
+        // foreach ($cartItems as $cartItem) {
+        //     $cartTotalQuantity += $cartItem->quantity;
+        // }
+
+        // // Get cart total price
+        // $cartTotalPrice = 0;
+        // foreach ($cartItems as $cartItem) {
+        //     $cartTotalPrice += $cartItem->total;
+        // }
+
+        // // Get cart total weight
+        // $cartTotalWeight = 0;
+        // foreach ($cartItems as $cartItem) {
+        //     $cartTotalWeight += $cartItem->product->weight * $cartItem->quantity;
+        // }
+
+        // // Get cart total volume
+        // $cartTotalVolume = 0;
+        // foreach ($cartItems as $cartItem) {
+        //     $cartTotalVolume += $cartItem->product->volume * $cartItem->quantity;
+        // }
+
+        foreach ($cart->items as $item) {
+            //$itemCartRuleIds = $this->process($item);
+        }
+
+    }
+
+    public function process(CartItem $item): array 
+    {
+        // Process cart item
+        //Log::info('Cart item processed '.json_encode($cartItem));
+        $item->discount_percent = 0;
+        $item->discount_amount = 0;
+        $item->base_discount_amount = 0;
+
+        $appliedRuleIds = [];
+
+        return $appliedRuleIds;
+        
+
     }
 }
